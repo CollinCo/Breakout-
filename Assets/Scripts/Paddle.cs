961890999a0dfc5b7ddb6c5a3acc637366adb1da
CollinCo <connollycollin@gmail.com>
2018-11-08 19:55:14 -0700

    Initial commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Paddle - bottom paddle object
+/// relies on mouse input for control
+/// </summary>
+public class Paddle : MonoBehaviour {
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthinUnits = 16f;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        //Debug.Log(Input.mousePosition);
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidthinUnits;
+        Vector2 paddlepos = new Vector2(mousePos, transform.position.y);
+        transform.position = paddlepos;
+    }
+
+}
---
+++ Assets/Scripts/tips.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tips : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cheer('lemao');
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    /// <summary>
+    /// Prints custom message to the console
+    /// </summary>
+    /// <param name="ayylmao">Ayylmao.</param>
+    void Cheer(string ayylmao) {
+        Debug.Log(ayylmao);
+    }
+}
